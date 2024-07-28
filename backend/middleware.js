b407015c47e6f90beed5ec23a8649dfb022ba231
sukritx <ddserial@gmail.com>
2024-07-29 04:09:31 +0700

    edit .env and middleware to import jwt from .env

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,5 +1,5 @@
-const { JWT_SECRET } = require("./config");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -12,7 +12,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
         next();
     } catch (err) {
