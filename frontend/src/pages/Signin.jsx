ff9f6f912b36cde04d399008f42a6f64d7e206c3
sukritx <ddserial@gmail.com>
2024-08-03 16:10:06 +0700

    remove localhost axios req

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -17,7 +17,7 @@ export const Signin = () => {
 
   const handleSignin = async () => {
     try {
-      const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+      const response = await axios.post("/api/v1/user/signin", {
         username,
         password
       });
