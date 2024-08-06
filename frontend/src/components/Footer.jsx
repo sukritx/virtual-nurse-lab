25738b9e52058414957ffa5fd092339b725c2742
sukritx <ddserial@gmail.com>
2024-08-07 02:47:39 +0700

    refactor: Update copyright year in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white py-4 text-center">
-        <p>© 2023 Virtual Nurse Lab. All rights reserved.</p>
+        <p>© 2024 Virtual Nurse Lab. All rights reserved.</p>
       </footer>
     );
   };
