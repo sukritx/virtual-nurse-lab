8a656b9616a3410a7446a81e82a35d43e09fff82
sukritx <ddserial@gmail.com>
2024-09-24 01:54:59 +0700

    fix bug

--- frontend/src/components/Lab-Reuse-Component/Lab2.jsx
+++ frontend/src/components/Lab-Reuse-Component/Lab2.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import axios from 'axios';
-import LabRecordingComponent from './LabRecordingComponent';
+import LabRecordingComponent from '../LabRecordingComponent';
 
 const MAX_ATTEMPTS = 3;
 
